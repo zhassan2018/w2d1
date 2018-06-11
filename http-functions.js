732bc9542790faa9f5102585db7a6addb521154c
Zohaib <zohaib.hassan@mail.mcgill.ca>
2018-06-11 18:14:22 +0000

    Step 6 files added

---
+++ http-functions.js
@@ -0,0 +1,24 @@
+module.exports = function getHTML (options, callback) {
+    /* Your code here */
+      var https = require('https');
+      var received =[];
+
+    https.get(options, function(response){
+    	response.on('data', function(data){
+    		received.push(data);
+    	})
+
+    	response.on('end', function(){
+    		callback(received)
+    	})
+    }
+
+    )
+
+
+
+};
+
+
+
+
